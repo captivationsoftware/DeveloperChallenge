13c92ba8e371dd0f550a7332b12c10492513a7b9
Connor Graff <connor.a.graff@gmail.com>
2021-02-18 18:54:44 -0500

    Completed assignment version 1

---
+++ CaptivationAssignment.py
@@ -0,0 +1,49 @@
+# Author: Connor Graff
+# Date: 2/18/2021
+# Purpose: This program will find CAPTIVATION in a series of 1's and 0's
+
+import sys
+# Import regular expressions
+import re
+
+# Create a variable holding the binary which translates to CAPTIVATION
+CAPTIVATION = "0100001101000001010100000101010001001001010101100100000101010100010010010100111101001110"
+# Create regex pattern to match against input. The pattern will look for the CAPTIVATION binary, then match the next 100 bits afterward
+pattern =  CAPTIVATION + '(.{100})'
+
+# Create the main function which holds the programs logic
+def main():
+    # Create a variable that will hold the entire series of 1's and 0's
+    fullSeriesOfBits = ""
+    while True:
+        # Call a function to get input and add it to the full series of bits
+        fullSeriesOfBits = addMoreFromSTDin(fullSeriesOfBits)
+        # Find all RegEx pattern matches in the series of bits compared to the pattern
+        x = re.findall(pattern, fullSeriesOfBits)
+        # If x exists, and the length of x is greater than 1 (There were multiple matches!)
+        if (x and len(x) > 1):
+            # Create a for loop to go through each one of the matches by index
+            for i in range(len(x)):
+                # If the ith index of x exists, and the ith index has a legnth of 100
+                if (x[i] and len(x[i]) == 100):
+                    # Print the 100 bits out
+                    sys.stdout.write(x[0])
+                    break
+            break
+        # Else if x exists, and the length of x is 1 (There was one match), and the 0th index of x exists, and the 0th index has a legnth of 100
+        elif(x and len(x) == 1 and x[0] and len(x[0]) == 100):
+            # Print the 100 bits out
+            sys.stdout.write(x[0])
+            break
+
+# Create a function to create/add to the full series of bits
+def addMoreFromSTDin(fullSeriesOfBits):
+    # Ask the user for input by an entire line
+    curChar = sys.stdin.readline()
+    # Add that line to the full series of bits
+    fullSeriesOfBits += curChar
+    # Return full series of bits
+    return fullSeriesOfBits
+
+# Run the main function
+main()
--- README.md
+++ README.md
@@ -1,21 +1,4 @@
-# Captivation Software Developer Challenge
+# Connor Graff - Captivation Software Developer Challenge
 
-## Your Task
-Develop an application that:
-- Reads a stream of "bits" (a continuous string consisting only of the character '0' and '1') from STDIN,
-- Decodes input characters into a single, zero-padded ASCII equivalent (e.g. "01000011" decodes to 'C')
-- Searches the decoded message for the preamble string "CAPTIVATION", and once found, prints the next one hundred decoded characters to STDOUT
-
-## Rules
-- The input stream will only ever consist of combinations of the character '0' or '1', no input validation is required
-- The input stream is to be treated as if it is never-ending
-- Multiple preamble/message occurrences may occur within the same input stream
-- Preamble/message occurrences are not guaranteed to be well-aligned (e.g. an arbitrary number of "bits" may precede a preamble, not just a multiple of 8)
-- The number of '0' and '1' characters between each preamble/message occurrence is variable 
-- Nothing else should be printed to STDOUT, only the one hundred characters following the preamble string "CAPTIVATION"
-
-## Guidelines
-- This solution will be tested by an automated tool, so failure to adhere to this spec precisely will produce a failing result 
-- Your solution should include instructions on how to run/build via Linux command line (or even better, use Docker)
-- Your solution will be judged for correctness, performance, and style
-- You may use any language you'd like, but you can only use standard libraries
+## How to test
+- Run, "py CaptivationAssignment.py", in your command line
